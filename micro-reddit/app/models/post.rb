11f7a6273bc33169ecdd1a588e6b3b4557bb3412
kaystep <kaystep@umich.edu>
2016-11-10 16:15:54 -0500

    Add files via upload

---
+++ micro-reddit/app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+	belongs_to :user
+	has_many :comments
+
+
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :body, presence: true, length: { minimum: 10 }
+	validates :user_id, presence: true
+end
